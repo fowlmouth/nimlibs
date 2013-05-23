359f0d4976ad3fcf053e9a887fc9258dbede7d66
Billingsly Wetherfordshire <phowl.mouth@gmail.com>
2013-05-22 21:06:56 -0500

    added bbtree and boundingbox modules

---
+++ fowltek/bbtree.nim
@@ -0,0 +1,212 @@
+import tables, fowltek/tmaybe, fowltek/boundingbox
+
+type
+  PBB_Node*[T] = ref TBB_Node[T] 
+  TBB_Node*[T] = object
+    parent: PBB_Node[T]
+    node_a, node_b: PBB_Node[T]
+    bb: TBB
+    obj: TMaybe[T]
+
+  PBB_Tree*[T] = var TBB_Tree[T]
+  TBB_Tree*[T] = object
+    items: TTable[T, PBB_Node[T]]
+    root: PBB_Node[T]
+
+
+proc newBBtree* [T] : TBB_Tree[T] =
+  result.items = initTable[T, PBB_Node[T]](512)
+proc newBBnode* [T](obj: T; bb: TBB): PBB_Node[T] {.
+  inline.} = PBB_Node[T](
+    obj: Just(obj), 
+    bb: bb)
+proc newBBnode* [T](bb: TBB): PBB_Node[T] =
+  new result
+  result.bb = bb
+ 
+proc a* [T](node: PBB_Node[T]): PBB_Node[T] {.inline.} = node.node_a
+proc `a=`*[T](node, val: PBB_Node[T]) {.inline.} =
+  node.node_a = val
+  node.node_a.parent = node
+proc b* [T](node: PBB_Node[T]): PBB_Node[T] {.inline.} = node.node_b
+proc `b=`*[T](node, val: PBB_Node[T]) {.inline.} = 
+  node.node_b = val
+  node.node_b.parent = node 
+
+proc isLeaf*[T] (node: PBB_Node[T]): bool {.inline.} = node.obj
+
+template otherchild (node, child): expr = (if node.a == child: node.b else: node.a)
+
+type E_SomeError* = object of E_Base
+
+
+proc updateBB* [T] (node: PBB_Node[T]) {.inline.}=
+  if not node.isLeaf:
+    var n = node
+    template refit : stmt = n.bb.refitFor(n.a.bb, n.b.bb)
+    while not n.isNil:
+      refit
+      n = n.parent
+      
+
+proc disownChild* [T] (node, leaf: PBB_Node[T]) =
+  let other = otherChild(node, leaf)
+  if node.parent.isLeaf: 
+    raise newException(E_SomeError, "Cannot replace the child of a leaf")
+  if not(node == node.parent.a or node == node.parent.b):
+    raise newException(E_SomeError, "AABBNode is not a child of parent") 
+  
+  if node.parent.a == node:
+    node.parent.a = other
+  else: 
+    node.parent.b = other
+
+  node.parent.updateBB
+
+proc removeSubtree*[T] (node, leaf: PBB_Node[T]): PBB_Node[T] =
+  if leaf == node: return nil
+  
+  if leaf.parent == node:
+    var otherChild = node.otherchild(leaf)
+    otherChild.parent = node.parent
+    return otherChild
+  
+  leaf.parent.disownChild leaf
+  return node
+
+proc proximity* [T] (node, leaf: PBB_Node[T]): float = (
+  (node.bb.left + node.bb.right - leaf.bb.left - leaf.bb.right).abs +
+  (node.bb.bottom + node.bb.top - leaf.bb.bottom - leaf.bb.top).abs)
+ 
+proc insertSubtree*[T] (node, leaf: PBB_Node[T]): PBB_Node[T] =
+  if node.isLeaf:
+    var n_n : PBB_node[T] #= newBBnode[T](node.bb.unionFast(leaf.bb))
+    new n_n
+    n_n.bb = node.bb.unionFast(leaf.bb)
+    n_n.a = node
+    n_n.b = leaf
+    return n_n
+
+  var 
+    cost_a = node.b.bb.area + node.a.bb.unionArea(leaf.bb)
+    cost_b = node.a.bb.area + node.b.bb.unionArea(leaf.bb)
+  
+  if cost_a == cost_b :
+    cost_a = node.a.proximity(leaf)
+    cost_b = node.b.proximity(leaf)
+  if cost_b < cost_a:
+    node.b = node.b.insertSubtree(leaf)
+  else:
+    node.a = node.a.insertSubtree(leaf)
+  
+  node.bb.expandToInclude leaf.bb
+  return node 
+
+proc insertLeaf*[T] (tree: PBB_Tree[T]; leaf: PBB_Node[T]) =
+  if not tree.root.isNil:
+    tree.root = insertSubtree[T](tree.root, leaf)
+  else:
+    tree.root = leaf
+
+proc remove*[T] (tree: PBB_Tree[T]; item: T) =
+  if tree.items.hasKey(item):
+    tree.root = tree.root.removeSubtree(tree.items[item]) 
+    tree.items.del item
+
+proc update*[T] (tree: PBB_Tree[T]; item: T; bb: TBB) =
+  let node = tree.items[item]
+  if not(node.isNil) and node.isLeaf:
+    if bb notin node.bb:
+      node.bb = bb
+      tree.root = tree.root.removeSubtree(node)
+      tree.insertLeaf node
+
+proc insert*[T] (tree: PBB_Tree[T]; item: T; bb: TBB) =
+  if tree.items.hasKey(item):
+    update[T](tree, item, bb)
+    return
+  var leaf = newBBnode[T](item, bb)
+  tree.items[item] = leaf
+  tree.insertLeaf leaf
+
+proc querySubtree* [T] (node: PBB_Node[T]; bb: TBB; cb: proc(x: T)) =
+  if node.bb.collidesWith(bb):
+    if not node.isLeaf:
+      node.a.querySubtree bb, cb
+      node.b.querySubtree bb, cb
+    else:
+      cb node.obj.val
+proc query* [T] (tree: PBB_Tree[T]; bb: TBB; cb: proc(x: T)) {.inline.} =
+  if tree.root.isNil: return
+  tree.root.querySubtree bb, cb
+
+
+when isMainModule:
+   
+  import fowltek/sdl2/engine
+  import_all_sdl2_modules
+  import_all_sdl2_helpers
+  
+  import colors, math
+  randomize()
+  
+  let colRed = colRed.toSDLcolor
+  
+  proc rectangleRGBA* (R: PRenderer, BB: TBB; col: sdl2.TColor) {.inline.} =
+    R.rectangleRGBA bb.left.int16, bb.top.int16, bb.right.int16, bb.bottom.int16,
+      col.R, col.G, col.B, col.A 
+  
+  proc debugDraw* [T] (some: PBB_Node[T]; R: PRenderer) =
+    R.rectangleRGBA some.bb, colRed
+    if not some.isLeaf:
+      some.a.debugDraw R
+      some.b.debugDraw R
+      
+  proc debugDraw* [T] (some: PBB_Tree[T]; r: PRenderer) = 
+    debugDraw some.root, r
+  
+  var NG = newSdlEngine()
+  let windowSize = NG.window.getSize
+  
+  var tree = newBBtree[int]()
+  var id = 0
+  
+  proc insert_a_box =
+    var box = bb(random(windowSize.x - 10) + 20, random(windowSize.y - 10) + 20,
+            random(500)+50, random(300)+50)
+    inc id
+    tree.insert id, box
+  
+  insert_a_box()
+  
+  var running = true
+  while running:
+    while NG.pollHandle:
+      case NG.evt.kind 
+      of QuitEvent:
+        running = false
+      of KeyDown:
+        let k = evKeyboard(NG.evt).keysym.sym
+        case k
+        of K_SPACE:
+          insert_a_box()
+        of K_M:
+          let box = bb(0,0,(windowSize.x / 2).int, (windowSize.y / 2).int)
+          tree.query box, proc(x: int) = 
+            echo "got ", x
+        
+        else:nil
+        
+      else:nil
+    
+    let dt = NG.frameDeltaFLT
+    
+    NG.setDrawColor 0,0,0,255
+    NG.clear
+    
+    tree.debugDraw NG
+    
+    NG.present
+  
+  NG.destroy
+
---
+++ fowltek/boundingbox.nim
@@ -0,0 +1,49 @@
+import math
+type
+  TBB* = tuple[
+    left, top, width, height: float]
+
+
+
+proc  bb* [T: TNumber] (x, y, w, h: T): TBB = (x.float, y.float, w.float, h.float) 
+
+proc right* (a: TBB): float {.inline.} = a.left + a.width
+proc bottom*(a: TBB): float {.inline.} = a.top + a.height
+proc area*  (a: TBB): float {.inline.} = a.width * a.height
+
+proc contains* (a, b: TBB): bool = not(
+  a.left > (b.right - 1) or
+  a.top > (b.bottom - 1) or
+  b.left > (a.right - 1) or 
+  b.top > (a.bottom - 1)               )
+
+
+proc unionArea* (a, b: TBB): float {.inline.} = (
+  (a.right.max(b.right) - a.left.min(b.left)) *
+  (a.bottom.max(b.bottom) - a.top.min(b.top))    )
+
+proc unionFast* (a, b: TBB): TBB =
+  let
+    rleft = a.left.min(b.left)
+    rtop = a.top.min(b.top)
+  return (rleft, rtop, a.right.max(b.right) - rleft, a.bottom.max(b.bottom) - rtop)
+
+proc expandToInclude* (bb: var TBB; b: TBB) =
+  let 
+    left = bb.left.min(b.left)
+    top = bb.top.min(b.top)
+  bb.left = left
+  bb.top = top
+  bb.width = bb.right.max(b.right) - left
+  bb.height = bb.bottom.max(b.bottom) - top 
+
+proc refitFor* (bb: var TBB; a, b: TBB) =
+  reset bb 
+  bb.expandToInclude a
+  bb.expandToInclude b
+
+proc collidesWith* (a, b: TBB): bool {.inline.} = (
+  ( (a.left >= b.left and a.left <= b.right) or (b.left >= a.left and b.left <= a.right) ) and
+  ( (a.top >= b.top and a.top <= b.bottom) or (b.top >= a.top and b.top <= a.bottom) ) )
+
+
