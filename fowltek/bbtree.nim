5948126816a4ed6978796616dd8494c93378a9a2
Fowl <phowl.mouth@gmail.com>
2013-06-04 02:14:12 -0700

    bbtree - add collision testing

--- fowltek/bbtree.nim
+++ fowltek/bbtree.nim
@@ -1,4 +1,5 @@
 import tables, fowltek/tmaybe, fowltek/boundingbox
+export tables, boundingbox, tmaybe
 
 type
   PBB_Node*[T] = ref TBB_Node[T] 
@@ -42,19 +43,19 @@ type E_SomeError* = object of E_Base
 
 proc updateBB* [T] (node: PBB_Node[T]) {.inline.}=
   if not node.isLeaf:
-    var n = node
-    template refit : stmt = n.bb.refitFor(n.a.bb, n.b.bb)
+    var n = node 
     while not n.isNil:
-      refit
+      n.bb.refitFor(n.a.bb, n.b.bb)
       n = n.parent
-      
+
 
 proc disownChild* [T] (node, leaf: PBB_Node[T]) =
-  let other = otherChild(node, leaf)
   if node.parent.isLeaf: 
     raise newException(E_SomeError, "Cannot replace the child of a leaf")
   if not(node == node.parent.a or node == node.parent.b):
-    raise newException(E_SomeError, "AABBNode is not a child of parent") 
+    raise newException(E_SomeError, "AABBNode is not a child of parent")
+  
+  template other: expr = otherChild(node, leaf)
   
   if node.parent.a == node:
     node.parent.a = other
@@ -129,17 +130,41 @@ proc insert*[T] (tree: PBB_Tree[T]; item: T; bb: TBB) =
   tree.items[item] = leaf
   tree.insertLeaf leaf
 
-proc querySubtree* [T] (node: PBB_Node[T]; bb: TBB; cb: proc(x: T)) =
+proc querySubtree* [T] (node: PBB_Node[T]; bb: TBB; cb: proc(item: T)) =
   if node.bb.collidesWith(bb):
     if not node.isLeaf:
       node.a.querySubtree bb, cb
       node.b.querySubtree bb, cb
     else:
       cb node.obj.val
-proc query* [T] (tree: PBB_Tree[T]; bb: TBB; cb: proc(x: T)) {.inline.} =
+proc query* [T] (tree: PBB_Tree[T]; bb: TBB; cb: proc(item: T)) {.inline.} =
   if tree.root.isNil: return
   tree.root.querySubtree bb, cb
 
+proc collectCollisions* [T] (tree: PBB_Tree[T]; bb: TBB; result: var seq[T]) {.inline.} =
+  if tree.root.isNil: return
+  let res = result.addr
+  tree.query(bb) do (item: T):
+    res[].add(item)
+
+proc collectCollisions* [T] (tree: PBB_Tree[T]; item: T; result: var seq[T]) {.inline.} =
+  if tree.root.isNil: return
+  let res = result.addr
+  tree.query(tree.items[item].bb) do (item2: T):
+    if item != item2: res[].add item2
+
+proc collectCollisions* [T] (tree: PBB_Tree[T]; bb: TBB|T): seq[T] {.inline.}=
+  result = @[]
+  tree.collectCollisions bb, result
+
+
+proc itemCount* [T] (tree: PBB_Tree[T]): int {.inline.} = tree.items.len
+
+## exposed for the test, mainly
+proc getObj* [T] (node: PBB_Node[T]): TMaybe[T] {.inline.} = node.obj
+proc getBB*  [T] (node: PBB_Node[T]): TBB {.inline.} = node.bb
+proc getRoot* [T] (tree: PBB_Tree[T]): PBB_Node[T] {.inline.} = tree.root
+proc hasItem* [T] (tree: PBB_Tree[T]; item: T): bool {.inline.} = tree.items.hasKey(item)
 
 when defined(usesdl2) or isMainModule:
   import fowltek/sdl2/engine, colors
@@ -154,11 +179,15 @@ when defined(usesdl2) or isMainModule:
   
   proc debugDraw* [T] (some: PBB_Node[T]; R: PRenderer; depth = 0) =
     var col = colRed
-    col.a = ((depth+7).min(13) * 19).uint8  
+    col.a = ((depth+10).min(13) * 19).uint8  
     R.rectangleRGBA some.bb, col
     if not some.isLeaf:
       some.a.debugDraw R, depth+1
       some.b.debugDraw R, depth+1
+
+    when compiles(debugDraw(some.bb, some.obj.val, R)):
+      if some.isLeaf:
+        debugDraw some.bb, some.obj.val, R
   
   proc debugDraw* [T] (some: PBB_Tree[T]; r: PRenderer) = 
     debugDraw some.root, r
@@ -176,13 +205,30 @@ when isMainModule:
   var tree = newBBtree[int]()
   var id = 0
   
+  proc debugDraw* (bb: TBB, item: int, R: PRenderer) =
+    R.stringRGBA bb.left.int16, bb.top.int16, $item, 0,255,0,255
+  
   proc insert_a_box =
     var box = bb(random(windowSize.x - 10) + 20, random(windowSize.y - 10) + 20, 
       random(400)+40, random(100)+20)
     inc id
     tree.insert id, box
   
-  insert_a_box()
+  #insert_a_box()
+  
+  block:
+    template ii (i, bb): stmt = tree.insert i, bb
+    ii 1,  bb(0, 0, 10, 10)
+    ii 2, bb(15, 0, 10, 20)
+    ii 3, bb(0, 15, 20, 10)
+    ii 4, bb(0, 50, 20, 5)
+    
+    #var bounds = tree.root.bb
+    #var rect = rect(0, 0, 100, 100)#( bounds.width + 10).cint, (bounds.height + 10).cint)
+    #echo rect
+    #NG.setViewport rect.addr
+    NG.setScale 4.0, 4.0
+
   
   var running = true
   while running:
@@ -200,7 +246,7 @@ when isMainModule:
         let k = evKeyboard(NG.evt).keysym.sym
         case k
         of K_SPACE:
-          insert_a_box()
+          #insert_a_box()
         
         else:nil
         
