d4e692f8d40260c22623bec8def821c261a1514d
Fowl <phowl.mouth@gmail.com>
2013-06-10 03:58:22 -0700

    added contains(TBB, TVector2[float]): bool

--- fowltek/boundingbox.nim
+++ fowltek/boundingbox.nim
@@ -14,10 +14,6 @@ proc right* (a: TBB): float {.inline.} = a.left + a.width
 proc bottom*(a: TBB): float {.inline.} = a.top + a.height
 proc area*  (a: TBB): float {.inline.} = a.width * a.height
 
-proc contains* (a, b: TBB): bool = (
-  a.left <= b.left and b.right <= a.right and
-  a.top <= b.top   and b.bottom <= a.bottom )
-
 proc unionArea* (a, b: TBB): float {.inline.} = (
   (a.right.max(b.right) - a.left.min(b.left)) *
   (a.bottom.max(b.bottom) - a.top.min(b.top))    )
@@ -46,4 +42,12 @@ proc collidesWith* (a, b: TBB): bool {.inline.} = (
   ( (a.left >= b.left and a.left <= b.right) or (b.left >= a.left and b.left <= a.right) ) and
   ( (a.top >= b.top   and a.top <= b.bottom) or (b.top >= a.top   and b.top <= a.bottom) ) )
 
+proc contains* (a, b: TBB): bool = (
+  a.left <= b.left and b.right <= a.right and
+  a.top <= b.top   and b.bottom <= a.bottom )
+
+from fowltek/vector_math import TVector2
+proc contains* (a: TBB; b: TVector2[float]): bool = (
+  b.x >= a.left and b.x <= a.right and
+  b.y >= a.top  and b.y <= a.bottom ) 
 
