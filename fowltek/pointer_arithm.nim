2c482e5c191bd3c2fad731e3854442c788b3907b
Fowl <phowl.mouth@gmail.com>
2013-06-25 10:22:25 -0700

    fixed a dumb error

--- fowltek/pointer_arithm.nim
+++ fowltek/pointer_arithm.nim
@@ -4,20 +4,20 @@ proc offset*[A](some: ptr A; b: int): ptr A =
 proc `+`*[A](some: ptr A, b: int): ptr A {.inline.} = some.offset(b)
 
 proc inc*[A](some: var ptr A, b = 1) =
-  some = some + b
+  some = some.offset(b)
 proc `++`*[A](some: var ptr A): ptr A =
   ## prefix inc
   result = some
-  inc some, 15
+  inc some
 
 
-proc alloc* [A](num: int = 1): ptr A {.
-  inline.} = cast[ptr A](alloc0(sizeof(A) * num))
+proc alloc* [A](num = 1): ptr A {.inline.} = 
+  cast[ptr A](alloc0(sizeof(A) * num))
 
-proc `[]`*[A](some: ptr A; idx: int): var A =
+proc `[]`*[A](some: ptr A; idx: int): var A {.inline.} =
   ## use offset() for tuple or array ptrs, this will not works
-  result = (some + idx)[]
-proc `[]=`*[A](some: ptr A; idx: int; val: A) =
+  some.offset(idx)[]
+proc `[]=`*[A](some: ptr A; idx: int; val: A) {.inline.} =
   (some[idx]) = val
 
 iterator iterPtr*[A](some: ptr A; num: int): ptr A =
