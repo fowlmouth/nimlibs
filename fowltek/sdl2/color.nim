b26ef7f94df6731d0fd4cdd135969fcb70887427
Fowl <phowl.mouth@gmail.com>
2013-10-16 15:02:05 -0700

    added a better game engine (with game state management)

--- fowltek/sdl2/color.nim
+++ fowltek/sdl2/color.nim
@@ -1,7 +1,8 @@
 import fowltek/sdl2, fowltek/vector_math
 
-
 import colors
+export colors
+
 proc toSdlColor*(c: colors.TColor): sdl2.TColor =
   ## Convert colors.TColor to SDL2.TColor
   let x = c.extractRGB  
@@ -10,8 +11,7 @@ proc toSdlColor*(c: colors.TColor): sdl2.TColor =
   result.b = x.b and 0xff
   result.a = 255
 
-
-
+  
 {.pragma: id, inline, discardable.}
 proc setDrawColor*(R: PRenderer; COL: sdl2.TColor): SDL_Return{.id.}=R.SetDrawColor(col.R, col.G, col.B, col.A) 
 
@@ -26,3 +26,19 @@ proc mlStringColor*(RE:PRenderer;X,Y: int16, S: string; COL: sdl2.TColor; LineSp
 proc mlStringColor*(RE:PRenderer;X,Y:int16;S:seq[string];COL: sdl2.TColor; LineSpacing = 2'i16): SDL_Return{.
   inline,discardable.}=mlStringRGBA(RE,X,Y,S,COL.R,COL.G,COL.B,COL.A,LineSpacing)
 
+
+#color packing for sdl2/gfx functions
+converter toint (c: colors.TColor): int = c.int
+proc pack* (c: colors.TColor; alpha = 255): uint32 =
+  let x = c.extractRGB
+  result = ((alpha and 0xFF) or 
+    (c shl 24  and 0xff) or 
+    (c shl 16  and 0xff) or
+    (c shl 8   and 0xff)).uint32 
+  var xz = [(x.r and 0xFF).uint8, (x.g and 0xFF).uint8, (x.b and 0xFF).uint8, (alpha and 0xFF).uint8]
+  result = cast[ptr uint32](xz[0].addr)[] 
+proc unpack* (c: uint32): sdl2.TColor = 
+  result.r = c shr 24 and 0xff
+  result.g = c shr 16 and 0xff
+  result.b = c shr 8  and 0xff
+  result.a = c and 0xff 
---
+++ fowltek/sdl2/engine2.nim
@@ -0,0 +1,136 @@
+import fowltek/sdl2, fowltek/sdl2/spriteCache
+
+template import_all_sdl2_modules*: stmt =
+  import fowltek/sdl2, fowltek/sdl2/image, fowltek/sdl2/gfx, fowltek/sdl2/ttf
+template import_all_sdl2_helpers*: stmt =
+  when not defined(toSDLcolor): 
+    import fowltek/sdl2/color
+  when not defined(spriteCache):
+    import fowltek/sdl2/spritecache
+template import_all_sdl2_things*: stmt =
+  import_all_sdl2_modules
+  import_all_sdl2_helpers
+
+type
+  PGameEngine* = var TGameEngine
+  TGameEngine* = object ##fuck it, this is actually "game engine", rename it later
+    gameStates: seq[PGameState]
+    lastTick: uint32
+    window*: PWindow
+    render*: PRenderer
+    running: bool
+    sprites: TSpriteCache
+  
+  PGameState* = ref object of TObject
+    update*: TGameStateUpdateCB
+    draw* :  TGameStateDrawCB
+    events* : seq[TGameStateEventCB]
+  
+  TGameStateUpdateCB* = proc(E: PGameEngine; dt: float)
+  TGameStateDrawCB* =  proc (E: PGameEngine)
+  TGameStateEventCB* = proc (E: PGameEngine; evt: var sdl2.TEvent): bool
+
+converter toRenderer* (some: PGameEngine): sdl2.PRenderer = some.render
+
+proc topState* (M: PGameEngine): PGameState {.inline.} =
+  M.gameStates[M.gameStates.high]
+
+
+proc frameDeltaMS*(some: PGameEngine): int32 {.
+  inline.} = 
+  ## Calculate the delta MS from the last frame.
+  ## This is no use unless you call it once a frame.
+  let cur = sdl2.getTicks()
+  result = int32(cur - some.lastTick)
+  some.lastTick = cur
+  
+proc frameDeltaFlt(some: PGameEngine): float {.
+  inline.} = some.frameDeltaMS / 1000
+
+proc handleEvents* (M: PGameEngine) {.inline.} =
+  var evt: sdl2.TEvent
+  while evt.pollEvent:
+    block tryToHandleIt:
+      for handler in M.topState.events:
+        if handler(M, evt): break tryToHandleIt
+
+proc draw* (M: PGameEngine) {.inline.} =
+  M.topState.draw M
+proc update* (M: PGameEngine) {.inline.} =
+  M.topState.update M, M.frameDeltaFLT()
+
+proc pushState* (M: PGameEngine; S: PGameState) =
+  M.gameStates.add S
+proc popState* (M: PGameEngine) =
+  discard M.gameStates.pop
+
+proc close* (M: PGameEngine) {.inline.} = 
+  M.running = false
+proc stop*  (M: PGameEngine) {.inline.} = 
+  M.close
+
+proc run * (M: PGameEngine) {.inline.} =
+  while M.running:
+    M.handleEvents
+    M.update 
+    
+    M.setDrawColor 0,0,0,255
+    M.clear
+    
+    M.draw 
+    
+    M.present
+
+proc newGameStateManager* (
+    gs: PGameState;
+    caption = "SDL Game", 
+    startX, startY = 100, 
+    sizeX = 640, sizeY = 480,
+    renderFlags = Renderer_Accelerated or Renderer_PresentVsync or Renderer_TargetTexture
+  ) : TGameEngine =
+  
+  discard sdl2.Init (INIT_EVERYTHING)
+  
+  result = TGameEngine(
+    gameStates: @[gs], 
+    running: true,
+    sprites: newSpriteCache(root = "assets"))
+  
+  result.window = CreateWindow(caption, startX.cint, startY.cint, 
+    sizeX.cint, sizeY.cint, SDL_WINDOW_SHOWN)
+  result.render = result.window.createRenderer(-1, 
+    Renderer_Accelerated or Renderer_PresentVsync or Renderer_TargetTexture)
+  
+  result.lastTick = sdl2.getTicks()
+  
+
+
+proc init* (gs: PGameState)  =
+  gs.draw = proc(E: PGameEngine) = nil
+  gs.update = proc(M: PGameEngine; dt: float) = nil
+  gs.events = @[]
+
+proc addHandler* (gs: PGameState; cb: TGameStateEventCB) {.inline.} =
+  gs.events.add cb
+
+
+
+proc closeOnQuitEvent* (M: PGameEngine; evt: var sdl2.TEvent): bool {.procvar.}=
+  result = evt.kind == QuitEvent
+  if result: M.close
+
+proc closeOnQuitEventOrKey* (key: cint): TGameStateEventCB =
+  return proc(M: PGameEngine; evt: var sdl2.TEvent): bool =
+    result = (evt.kind == QuitEvent) or 
+      (evt.kind == KeyDown and evt.EvKeyboard.keysym.sym == key)
+    if result: M.close
+
+proc keyEvent* (
+    key: cint; 
+    evtKind: TEventType; 
+    action: proc(M: PGameEngine)): TGameStateEventCB =
+  assert evtKind in {KeyDown, KeyUP}
+  return proc(M: PGameEngine; evt: var sdl2.TEvent): bool =
+    result = evt.kind == evtKind and 
+      evt.EvKeyboard.keysym.sym == key
+    if result: action(M)
